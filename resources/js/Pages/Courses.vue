7a21130e352809fda5ec27b6e8007ac527000566
Seth Sharp <sesharp@outlook.com>
2022-10-04 11:17:58 +1000

    Authenticated layout and custom background colour and gradient

--- resources/js/Pages/Courses.vue
+++ resources/js/Pages/Courses.vue
@@ -1,14 +1,18 @@
 <script setup>
-
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
     let props = defineProps(['courses']);
-
 </script>
 
 <style> </style>
 
 <template>
-    <h1> This is courses page</h1>
-    <div v-for="course in courses">
-        {{ course.title }}
+    <div class="bg-gradient-to-br from-backgroundFrom to-backgroundTo">
+        <AuthenticatedLayout>
+            <h1> This is courses page</h1>
+            <div v-for="course in courses">
+                {{ course.title }}
+            </div>
+        </AuthenticatedLayout>
     </div>
+
 </template>
