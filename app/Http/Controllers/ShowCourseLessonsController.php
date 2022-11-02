8e0bcf7b70ac39a542e5b26f10106fe0531f5a5e
Seth Sharp <sesharp@outlook.com>
2022-11-02 16:27:43 +1000

    Rough try catch for invalid course names

--- app/Http/Controllers/ShowCourseLessonsController.php
+++ app/Http/Controllers/ShowCourseLessonsController.php
@@ -4,18 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Lesson;
+use Exception;
 use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
 class ShowCourseLessonsController extends Controller
 {
     public function __invoke($course_name)
     {
-        $course = Course::firstwhere('name', $course_name);
-        $lessons = Lesson::where('course_id', $course->id)->get();
-        return Inertia('Lessons', [
-            // TODO: name as slug (not id)
-            'name' => $course->name,
-            'lessons' => $lessons,
-            'user' => Auth::check()
-        ]);
+        try {
+            $course = Course::firstwhere('name', $course_name);
+            $lessons = Lesson::where('course_id', $course->id)->get();
+            return Inertia('Lessons', [
+                'name' => $course->name,
+                'lessons' => $lessons,
+                'user' => Auth::check()
+            ]);
+        } catch (Exception $e) {
+            return Inertia('ErrorPage', [
+                'error' => "Course could not be found or does not exist"
+            ]);
+        }
     }
 }
---
+++ resources/js/Pages/ErrorPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import Header from '@/Layouts/Header.vue';
+    const props = defineProps(['error']);
+</script>
+
+<template>
+    <Header>
+        <div class="text-white">
+            {{ error }}
+        </div>
+    </Header>
+</template>
